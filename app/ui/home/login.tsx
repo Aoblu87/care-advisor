3458136447850cc53ec208234e17e0faaad17c69
Stefania <stefania.struzzi@gmail.com>
2023-12-29 18:28:27 +0100

    &apos; aggiunto

--- app/ui/home/login.tsx
+++ app/ui/home/login.tsx
@@ -49,7 +49,7 @@ export default function Login() {
                   Sign in
                 </h1>
                 <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-                  Don't have an account yet?
+                  Don&apos;t have an account yet?
                   <a className="text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                     {/* <SignUp /> Sign up here */}
                   </a>
