b9f81f6a4e23a1f9022901c3c9bbe8f9394758c8
Stefania <stefania.struzzi@gmail.com>
2024-01-04 13:03:50 +0100

    modificata rpoute.ts API

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,10 @@
 import connectionDB from "@/app/lib/connectionDB.js";
-import type { NextApiResponse } from "next";
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 type ResponseData = {
   message: string;
 };
 connectionDB();
-export function GET(req: NextRequest, response: NextApiResponse<ResponseData>) {
-  response.json({ message: "Hello from Next.js!" });
+export function GET() {
+  return NextResponse.json({ message: "Hello from Next.js!" });
 }
